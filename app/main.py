27eb58e01675a48d2462e0bdd49aac5a7fcd1163
GrozaMorey <yorik294@gmail.com>
2023-09-29 12:19:26 +0300

    2

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 import socket
 
-
+#KEK
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, adress = server_socket.accept()
+
+    with conn:
+        conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
     main()
-    
