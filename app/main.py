35713c34e6d6e2bfd8b2bfcdc49178a781cefb84
GrozaMorey <yorik294@gmail.com>
2023-10-04 17:28:17 +0300

    try pass 6 stage

--- app/main.py
+++ app/main.py
@@ -1,24 +1,25 @@
 import socket
 import re
+import threading
 
+def handle_client(conn, adress):
+    print("connect by", adress)
 
-def main():
-    global conn
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    conn, adress = server_socket.accept()
 
     with conn:
         request = Request(conn.recv(1024).decode())
         if request.path == "/":
             conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
+
         elif re.match("/echo/*", request.path):
             path = request.path.replace("/echo/", "")
             response = bytes(
                 f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path)}\r\n\r\n{path}",
                 encoding="UTF-8", )
             conn.send(response)
+
         elif re.match("/user-agent/*", request.path):
-            Response(request.user_agent).send()
+            Response(request.user_agent).send(conn)
         else:
             conn.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
@@ -42,9 +43,17 @@ class Request:
                 continue
 
 
+def main():
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+
+    while True:
+        conn, adress = server_socket.accept()
+        threading.Thread(target=handle_client, args=(conn,adress)).start()
+
+
 class Response:
 
-    def __init__(self, body, code=200):
+    def __init__(self, body, code=200,):
         if type(body) is str:
             self.code = code
 
@@ -52,7 +61,7 @@ class Response:
             self.content_length = len(body)
             self.body = body
 
-    def send(self):
+    def send(self, conn):
         conn.send(bytes(f"HTTP/1.1 {self.code} OK\r\n"
                         f"Content-Type: text/plain\r\n"
                         f"Content-Length: {self.content_length}\r\n\r\n{self.body}",
@@ -60,5 +69,5 @@ class Response:
                   )
 
 
-while True:
+if __name__ == "__main__":
     main()
