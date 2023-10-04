84e9ec9f90be9fe5809fcc6150879116ad2d2c24
GrozaMorey <yorik294@gmail.com>
2023-10-04 11:53:15 +0300

    try pass 5 stage

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 import socket
 import re
+
+
 def main():
+    global conn
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, adress = server_socket.accept()
 
@@ -10,14 +13,17 @@ def main():
             conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
         elif re.match("/echo/*", request.path):
             path = request.path.replace("/echo/", "")
-            response = bytes(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path)}\r\n\r\n{path}", encoding="UTF-8",)
+            response = bytes(
+                f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path)}\r\n\r\n{path}",
+                encoding="UTF-8", )
             conn.send(response)
+        elif re.match("/user-agent/*", request.path):
+            Response(request.user_agent).send()
         else:
             conn.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 
 class Request:
-
     def __init__(self, data):
         data = data.split("\r\n")
         self.path = data[0].split(" ")[1]
@@ -30,14 +36,28 @@ class Request:
             try:
                 i = i.split(": ")
 
-                setattr(self, i[0].lower().replace(" ", ""), i[1].lower().replace(" ", ""))
+                setattr(self, i[0].lower().replace(" ", "").replace("-", "_"), i[1].lower().replace(" ", ""))
 
             except IndexError:
                 continue
 
 
+class Response:
 
+    def __init__(self, body, code=200):
+        if type(body) is str:
+            self.code = code
 
+        if body:
+            self.content_length = len(body)
+            self.body = body
+
+    def send(self):
+        conn.send(bytes(f"HTTP/1.1 {self.code} OK\r\n"
+                        f"Content-Type: text/plain\r\n"
+                        f"Content-Length: {self.content_length}\r\n\r\n{self.body}",
+                        encoding="UTF-8", )
+                  )
 
 
 while True:
