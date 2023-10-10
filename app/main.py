e4e7de3f7f52caf0ee903fddf5bf9eebe5effbf2
GrozaMorey <yorik294@gmail.com>
2023-10-10 03:56:38 +0300

    try pass 7 stage

--- app/main.py
+++ app/main.py
@@ -88,7 +88,7 @@ class Response:
         conn.send(f"HTTP/1.1 200 OK\r\n"
                         f"Content-Type: {self.content_type}\r\n"
                         f"Content-Length: {self.content_length}\r\n\r\n{self.body}"
-                  )
+                  ).encode()
         print("response was success send")
 
 
